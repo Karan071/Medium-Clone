fc92f561f0ca42db277d9a7260585d828cded873
Karan <kc2532002@gmail.com>
2025-05-18 00:35:34 +0530

    backend-connect

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,4 +1,4 @@
-export const BACKEND_URL="http://localhost:8787"
+export const BACKEND_URL="https://backend.kc2532002.workers.dev"
 export const REACT_APP_QUOTE_API_KEY="DHyQcGhAD5UaSfZnPTVk1A==v7V5Yvj9alO96rZl"
 
 
